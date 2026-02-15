844b757a6be61c23fd9495c7141ef3fb3368fddd
Am4teratsu <mathiasporrasvilca2705@gmail.com>
2026-02-15 12:14:07 -0500

    Proyecto Inicial

---
+++ src/main/java/com/hotel/demo/MiyabiApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiyabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiyabiApplication.class, args);
+	}
+
+}
